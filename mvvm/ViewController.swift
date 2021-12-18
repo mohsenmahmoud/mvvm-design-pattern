c69f334c1e6c82fc909261bb96612e05fcc194db
NawayaGit <mohsenmahmoud490@gmail.com>
2021-12-18 23:02:45 +0200

    Initial Commit

---
+++ mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by mohsen on 18/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
